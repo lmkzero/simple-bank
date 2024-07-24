e2d3506158589a2b65916d3f83e79ad45a4c44ba
mengkailiu <dave_lmk@outlook.com>
2024-07-24 23:59:37 +0800

    transfer transaction - add transfer records

---
+++ internal/data/store.go
@@ -0,0 +1,19 @@
+// Package data 封装db方法
+package data
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Store 提供封装的db方法和事务
+type Store struct {
+	pool *pgxpool.Pool
+}
+
+// NewStore 工厂方法
+// * sqlc配置中使用sql_package="pgx/v5"，所以这里使用database/sql接口会冲突
+func NewStore(pool *pgxpool.Pool) *Store {
+	return &Store{
+		pool: pool,
+	}
+}
---
+++ internal/data/transaction.go
@@ -0,0 +1,85 @@
+package data
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lmkzero/simple-bank/internal/data/db"
+)
+
+func (s *Store) execTransaction(ctx context.Context, f func(*db.Queries) error) error {
+	tx, err := s.pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	if err := f(db.New(tx)); err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("exec transaction: %w, rollback: %w", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+// TransferTx 转账事务
+func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResults, error) {
+	results := TransferTxResults{}
+	if err := s.execTransaction(ctx, func(q *db.Queries) error {
+		transfer, err := q.CreateTransfer(
+			ctx,
+			db.CreateTransferParams{
+				FromAccountID: arg.FromAccountID,
+				ToAccountID:   arg.ToAccountID,
+				Amount:        arg.Amount,
+			},
+		)
+		if err != nil {
+			return err
+		}
+		fromEntry, err := q.CreateEntry(
+			ctx,
+			db.CreateEntryParams{
+				AccountID: arg.FromAccountID,
+				Amount:    -arg.Amount,
+			},
+		)
+		if err != nil {
+			return err
+		}
+		toEntry, err := q.CreateEntry(
+			ctx,
+			db.CreateEntryParams{
+				AccountID: arg.ToAccountID,
+				Amount:    arg.Amount,
+			},
+		)
+		if err != nil {
+			return err
+		}
+		// TODO 更新两个账户的余额
+		// 赋值转账事务结果
+		results.Transfer = transfer
+		results.FromEntry = fromEntry
+		results.ToEntry = toEntry
+		return nil
+	}); err != nil {
+		return results, err
+	}
+	return results, nil
+}
+
+// TransferTxParams 转账事务入参
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResults 转账事务结果
+type TransferTxResults struct {
+	Transfer    db.Transfer `json:"transfer"`
+	FromAccount db.Account  `json:"from_account"`
+	ToAccount   db.Account  `json:"to_account"`
+	FromEntry   db.Entry    `json:"from_entry"`
+	ToEntry     db.Entry    `json:"to_entry"`
+}
